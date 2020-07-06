bb2b1345749c57fb7b1d257fecb38252bd3a8bda
John Callaway <ovation22@users.noreply.github.com>
2020-07-06 18:58:06 -0400

    In-Memory Repository Tests (#3)
    Co-authored-by: John Callaway <john@comcept.net>

---
+++ SpeakerMeet.Core/Entities/Speaker.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpeakerMeet.Core.Entities
+{
+    public class Speaker
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
