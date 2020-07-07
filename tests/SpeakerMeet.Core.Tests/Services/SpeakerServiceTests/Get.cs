c90a78e2edd14187c556832e6951055fb6cd6ce7
John Callaway <ovation22@users.noreply.github.com>
2020-07-07 09:50:47 -0400

    Feature/service tests (#12)
    * Speaker Service Tests

---
+++ tests/SpeakerMeet.Core.Tests/Services/SpeakerServiceTests/Get.cs
@@ -0,0 +1,19 @@
+﻿using SpeakerMeet.Core.Services;
+using Xunit;
+
+namespace SpeakerMeet.Core.Tests.Services.SpeakerServiceTests
+{
+    public class Get
+    {
+        [Fact]
+        public void ItExists()
+        {
+            // Arrange
+            // Act
+            var service = new SpeakerService();
+
+            // Assert
+            Assert.NotNull(service);
+        }
+    }
+}
---
+++ tests/SpeakerMeet.Core.Tests/Services/SpeakerServiceTests/GetAll.cs
@@ -0,0 +1,19 @@
+﻿using SpeakerMeet.Core.Services;
+using Xunit;
+
+namespace SpeakerMeet.Core.Tests.Services.SpeakerServiceTests
+{
+    public class GetAll
+    {
+        [Fact]
+        public void ItExists()
+        {
+            // Arrange
+            // Act
+            var service = new SpeakerService();
+
+            // Assert
+            Assert.NotNull(service);
+        }
+    }
+}
