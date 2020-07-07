072d0845d1e80f5e9c742294e0735dbde1297449
John Callaway <ovation22@users.noreply.github.com>
2020-07-06 21:54:25 -0400

    Code Coverage (#7)
    * Code Coverage

--- src/SpeakerMeet.Core/Interfaces/Repositories/IEFRepository.cs
+++ src/SpeakerMeet.Core/Interfaces/Repositories/IEFRepository.cs
@@ -11,5 +11,8 @@ namespace SpeakerMeet.Core.Interfaces.Repositories
         Task<IEnumerable<T>> GetAll<T>() where T : class;
         Task<IEnumerable<T>> GetAll<T>(Expression<Func<T, bool>> expression) where T : class;
         Task<IEnumerable<T>> GetAll<T>(Expression<Func<T, bool>> expression, Expression<Func<T, object>> include) where T : class;
+        Task<T> Add<T>(T entity) where T : class;
+        Task Update<T>(T entity) where T : class;
+        Task Delete<T>(T entity) where T : class;
     }
 }
