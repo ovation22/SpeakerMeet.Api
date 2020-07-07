3844c02d18aea17b65cd28b0bc79013e989d46d1
John Callaway <ovation22@users.noreply.github.com>
2020-07-06 22:47:39 -0400

    More Controller Tests (#10)

---
+++ tests/SpeakerMeet.Api.Tests/Controllers/CommunitiesControllerTests/GetAll.cs
@@ -0,0 +1,21 @@
+﻿using SpeakerMeet.Api.Controllers;
+using Xunit;
+
+namespace SpeakerMeet.Api.Tests.Controllers.CommunitiesControllerTests
+{
+    public class GetAll
+    {
+        [Fact]
+        public void ItReturns()
+        {
+            // Arrange
+            var controller = new CommunitiesController();
+
+            // Act
+            var result = controller.GetAll();
+
+            // Assert
+            Assert.IsAssignableFrom<string[]>(result);
+        }
+    }
+}
---
+++ tests/SpeakerMeet.Api.Tests/Controllers/ConferencesControllerTests/GetAll.cs
@@ -0,0 +1,21 @@
+﻿using SpeakerMeet.Api.Controllers;
+using Xunit;
+
+namespace SpeakerMeet.Api.Tests.Controllers.ConferencesControllerTests
+{
+    public class GetAll
+    {
+        [Fact]
+        public void ItReturns()
+        {
+            // Arrange
+            var controller = new ConferencesController();
+
+            // Act
+            var result = controller.GetAll();
+
+            // Assert
+            Assert.IsAssignableFrom<string[]>(result);
+        }
+    }
+}
