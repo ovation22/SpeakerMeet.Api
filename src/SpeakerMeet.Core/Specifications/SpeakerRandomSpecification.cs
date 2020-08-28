9a155867330f537f79960c10affdafc0db1d1881
John Callaway <ovation22@users.noreply.github.com>
2020-08-28 12:53:17 -0400

    Paginate Take (#44)

--- src/SpeakerMeet.Core/Specifications/SpeakerRandomSpecification.cs
+++ src/SpeakerMeet.Core/Specifications/SpeakerRandomSpecification.cs
@@ -4,16 +4,15 @@ using SpeakerMeet.Core.Entities;
 
 namespace SpeakerMeet.Core.Specifications
 {
-    public class SpeakerRandomSpecification : Specification<Speaker>
+    public sealed class SpeakerRandomSpecification : Specification<Speaker>
     {
         public SpeakerRandomSpecification()
         {
-            Take = 4;
             Query
                 .Where(x => x.IsActive)
                 .OrderBy(x => Guid.NewGuid());
-        }
 
-        public new int Take { get; internal set; }
+            Query.Paginate(0, 4);
+        }
     }
 }
