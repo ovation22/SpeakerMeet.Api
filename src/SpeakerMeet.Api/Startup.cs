3dc6bb97bf8e0550b44ac56c9d1841ceadb946b4
ovation22 <john@1north.com>
2020-10-07 21:05:59 -0400

    docker-compose

--- src/SpeakerMeet.Api/Startup.cs
+++ src/SpeakerMeet.Api/Startup.cs
@@ -64,7 +64,7 @@ namespace SpeakerMeet.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsEnvironment("Local"))
             {
                 app.UseDeveloperExceptionPage();
             }
