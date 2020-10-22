92ba8566222cbc1692f06bdd56d85b81fb44e0dd
ovation22 <john@1north.com>
2020-10-21 22:55:20 -0400

    Integration Tests with the DB using Docker

---
+++ tests/SpeakerMeet.Tests.Integration.DB/ApiTestCollection.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace SpeakerMeet.Tests.Integration.DB
+{
+    [CollectionDefinition(CollectionName)]
+    public class ApiTestCollection : ICollectionFixture<ApiTestContext>
+    {
+        public const string CollectionName = "API Test";
+    }
+}
